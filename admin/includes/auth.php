f9655ad1dabd39b909f08db6c75718904d651e6c
Dr Adnan Jabbar <dradnanjabbar@gmail.com>
2026-02-26 02:06:17 +0500

    Fix semen module crash

--- admin/includes/auth.php
+++ admin/includes/auth.php
@@ -1,7 +1,10 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 if (!isset($_SESSION['admin'])) {
-    header("Location: login.php");
+    header("Location: /admin/login.php");
     exit();
 }
 ?>
